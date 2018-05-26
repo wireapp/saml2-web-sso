78fbfa7aeb4fda9bb458604e146eea685aaf85da
Matthias Fischmann <mf@zerobuzz.net>
2018-05-26 12:02:59 +0200

    Fix: metadata.

--- src/SAML2/WebSSO/XML/Meta.hs
+++ src/SAML2/WebSSO/XML/Meta.hs
@@ -1,5 +1,6 @@
 module SAML2.WebSSO.XML.Meta
   ( SPDescPre(..), SPDesc(..)
+  , spdID
   , spdValidUntil
   , spdCacheDuration
   , spdOrgName
@@ -27,10 +28,13 @@ import Text.XML.Cursor
 import Text.XML.Util
 import URI.ByteString
 
+import qualified Data.UUID as UUID
 import qualified Network.URI as OldURI
 import qualified SAML2.Bindings.Identifiers as HS
 import qualified SAML2.Core.Datatypes as HS hiding (AnyURI)
 import qualified SAML2.Core.Identifiers as HS
+import qualified SAML2.Core.Namespaces as HS
+import qualified SAML2.Core.Versioning as HS
 import qualified SAML2.Metadata.Metadata as HS
 import qualified SAML2.XML as HX hiding (AnyURI)
 import qualified SAML2.XML.Schema.Datatypes as HX
@@ -52,7 +56,8 @@ instance HasXMLRoot SPDesc where
 -- | high-level, condensed data uesd for constructing an 'SPDesc'.  what is not in here is set to
 -- some constant default.
 data SPDescPre = SPDescPre
-  { _spdValidUntil     :: UTCTime          -- FUTUREWORK: Time
+  { _spdID             :: UUID.UUID
+  , _spdValidUntil     :: UTCTime          -- FUTUREWORK: Time
   , _spdCacheDuration  :: NominalDiffTime  -- FUTUREWORK: Duration
   , _spdOrgName        :: ST
   , _spdOrgDisplayName :: ST
@@ -65,8 +70,12 @@ makeLenses ''SPDescPre
 
 
 spDesc :: SP m => ST -> URI -> URI -> m SPDescPre
-spDesc nick org resp = do
-  let _spdCacheDuration  = months 1
+spDesc nick org resp = createUUID >>= \uuid -> spDesc' uuid nick org resp
+
+spDesc' :: SP m => UUID.UUID -> ST -> URI -> URI -> m SPDescPre
+spDesc' uuid nick org resp = do
+  let _spdID             = uuid
+      _spdCacheDuration  = months 1
       _spdOrgName        = nick
       _spdOrgDisplayName = nick
       _spdOrgURL         = org
@@ -84,14 +93,14 @@ spDesc nick org resp = do
 spMeta :: HasCallStack => SPDescPre -> SPDesc
 spMeta spdesc = either (error . show) SPDesc . parseLBS def . HX.samlToXML $ spMeta' spdesc
 
--- | [4/2.6]
+-- | [4/2.6], [4/2]
 spMeta' :: HasCallStack => SPDescPre -> HS.Descriptor
 spMeta' spdesc = HS.SPSSODescriptor
     { HS.descriptorRole = HS.RoleDescriptor
-      { HS.roleDescriptorID = Just "e3a565aa-1392-4446-a4d6-3771453808f0" :: Maybe HS.ID
+      { HS.roleDescriptorID = Just (UUID.toString $ spdesc ^. spdID) :: Maybe HS.ID
       , HS.roleDescriptorValidUntil = Just (spdesc ^. spdValidUntil) :: Maybe HS.DateTime
       , HS.roleDescriptorCacheDuration = Just (spdesc ^. spdCacheDuration) :: Maybe HX.Duration
-      , HS.roleDescriptorProtocolSupportEnumeration = [fromJust $ OldURI.parseURI "urn:oasis:names:tc:SAML:2.0:protocol"] :: [HX.AnyURI]
+      , HS.roleDescriptorProtocolSupportEnumeration = [HS.samlURN HS.SAML20 ["protocol"]] :: [HX.AnyURI]
       , HS.roleDescriptorErrorURL = Nothing :: Maybe HX.AnyURI
       , HS.roleDescriptorAttrs = [] :: HX.Nodes
       , HS.roleDescriptorSignature = Nothing :: Maybe HX.Signature
@@ -119,7 +128,7 @@ spMeta' spdesc = HS.SPSSODescriptor
       { HS.ssoDescriptorArtifactResolutionService = [] :: [HS.IndexedEndpoint]
       , HS.ssoDescriptorSingleLogoutService = [] :: [HS.Endpoint]
       , HS.ssoDescriptorManageNameIDService = [] :: [HS.Endpoint]
-      , HS.ssoDescriptorNameIDFormat = [ {- "urn:oasis:names:tc:SAML:1.1:nameid-format:unspecified" -}] :: [HX.IdentifiedURI HS.NameIDFormat]  -- TODO
+      , HS.ssoDescriptorNameIDFormat = [HX.Identified HS.NameIDFormatUnspecified, HX.Identified HS.NameIDFormatEntity]  -- [1/8.3]
       }
     , HS.descriptorAuthnRequestsSigned = False
     , HS.descriptorWantAssertionsSigned = True
--- test/Test/SAML2/WebSSO/XML/MetaSpec.hs
+++ test/Test/SAML2/WebSSO/XML/MetaSpec.hs
@@ -2,7 +2,9 @@
 
 module Test.SAML2.WebSSO.XML.MetaSpec (spec) where
 
+import Control.Lens
 import Data.EitherR
+import Data.Maybe (fromJust)
 import Data.String.Conversions
 import Data.Time
 import SAML2.WebSSO
@@ -12,31 +14,32 @@ import Text.XML
 import Text.XML.Util
 import Util
 
+import qualified Data.UUID as UUID
+
+
 spec :: Spec
 spec = do
   describe "spDesc" $ do
     it "does not smoke" $ do
       have <- testSP testCtx1 $ spDesc "drnick" (unsafeParseURI "http://example.com/") (unsafeParseURI "http://example.com/sso/login")
-      let want = SPDescPre
-            { _spdValidUntil = addUTCTime (60 * 60 * 24 * 365) $ fromTime timeNow
-            , _spdCacheDuration = 2592000
-            , _spdOrgName = "drnick"
-            , _spdOrgDisplayName = "drnick"
-            , _spdOrgURL = unsafeParseURI "http://example.com/"
-            , _spdResponseURL = unsafeParseURI "http://example.com/sso/login"
-            }
+      let want = spdescpre (have ^. spdID)
       have `shouldBe` want
 
   describe "spMeta" $ do
     it "does not smoke" $ do
-      let given = SPDescPre
-            { _spdValidUntil = addUTCTime (60 * 60 * 24 * 365) $ fromTime timeNow
-            , _spdCacheDuration = 2592000
-            , _spdOrgName = "drnick"
-            , _spdOrgDisplayName = "drnick"
-            , _spdOrgURL = unsafeParseURI "http://example.com/"
-            , _spdResponseURL = unsafeParseURI "http://example.com/sso/login"
-            }
+      let given = spdescpre . fromJust $ UUID.fromText "e3a565aa-1392-4446-a4d6-3771453808f0"
           SPDesc (want :: Document) = spMeta given
-          have :: Either String Document = fmapL show . parseText def . cs $ readSample "our-spssodescriptor.xml"
+      have :: Either String Document <- fmapL show . parseText def . cs <$> readSampleIO "our-spssodescriptor.xml"
       have `shouldBe` Right want
+
+
+spdescpre :: UUID.UUID -> SPDescPre
+spdescpre uuid = SPDescPre
+  { _spdID = uuid
+  , _spdValidUntil = addUTCTime (60 * 60 * 24 * 365) $ fromTime timeNow
+  , _spdCacheDuration = 2592000
+  , _spdOrgName = "drnick"
+  , _spdOrgDisplayName = "drnick"
+  , _spdOrgURL = unsafeParseURI "http://example.com/"
+  , _spdResponseURL = unsafeParseURI "http://example.com/sso/login"
+  }
