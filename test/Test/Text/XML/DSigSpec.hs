080dd956c792fca401c3246fffee2061e69d3c77
Matthias Fischmann <mf@zerobuzz.net>
2018-08-13 14:59:18 +0200

    Cache public keys in test suite for faster test runs.

--- test/Test/Text/XML/DSigSpec.hs
+++ test/Test/Text/XML/DSigSpec.hs
@@ -7,6 +7,7 @@ module Test.Text.XML.DSigSpec (spec) where
 import Control.Monad ((>=>))
 import Data.Either
 import Data.String.Conversions
+import Samples (pubA, privA, pubB)
 import Test.Hspec
 import Text.Hamlet.XML (xml)
 import Text.XML
@@ -55,12 +56,20 @@ spec = describe "xml:dsig" $ do
         check withMatchingCreds withID expected =
           it (show (withMatchingCreds, withID, expected)) $ do
             (privCreds, pubCreds) <- mkcrds withMatchingCreds
+            putStrLn . cs . renderLBS def . either error id $ signRoot privCreds (doc withID)
             (verifyRoot pubCreds . renderLBS def =<< signRoot privCreds (doc withID)) `shouldBe` expected
 
-        keysize = 192  -- not long enough for security, but hopefully long enough for swift testing
-        mkcrds = \case
+        mkcrds, _mkcrdsReal, _mkcrdsCached :: Bool -> IO (SignPrivCreds, SignCreds)
+        mkcrds = _mkcrdsCached
+
+        _mkcrdsReal = \case
           True  -> mkSignCreds keysize
           False -> (,) <$> (fst <$> mkSignCreds keysize) <*> (snd <$> mkSignCreds keysize)
+        keysize = 192  -- not long enough for security, but hopefully long enough for swift testing
+
+        _mkcrdsCached = pure . \case
+          True  -> (privA, pubA)
+          False -> (privA, pubB)
 
         someID withID = Map.fromList [("ID", UUID.toText UUID.nil) | withID]
         doc withID = Document (Prologue [] Nothing []) (Element "root" (someID withID) root) []
