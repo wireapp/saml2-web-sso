3ce24899d05592ffd6180bb9225d429173af5691
Matthias Fischmann <mf@zerobuzz.net>
2018-10-31 14:56:40 +0100

    Better hedgehog xml roundtrip tests.

--- test/Test/SAML2/WebSSO/XML/RoundtripSpec.hs
+++ test/Test/SAML2/WebSSO/XML/RoundtripSpec.hs
@@ -2,37 +2,75 @@
 
 module Test.SAML2.WebSSO.XML.RoundtripSpec (spec) where
 
-import SAML2.WebSSO.Test.Arbitrary
+import Control.Lens
+import Data.List
+import Data.Proxy
 import Hedgehog
 import Hedgehog.Gen as Gen
 import SAML2.WebSSO
+import SAML2.WebSSO.Test.Arbitrary
 import Test.Hspec
 import Util
+
+import qualified Data.List.NonEmpty as NL
+import qualified SAML2.Core as HS
 
 
 spec :: Spec
 spec = hedgehog $ checkParallel $$(discover)
 
-mkprop :: forall a. (Eq a, Show a, HasXML a) => Gen a -> Property
-mkprop gen = property $ forAll gen >>= \v -> tripping v enc dec
-  where
-    (enc, dec) =
-      (encodeElem, decodeElem @a @(Either String))
-      -- (render, (parse @a @(Either String)))
+numTests :: TestLimit
+numTests = 120
+
+mkpropHasXML
+  :: forall a. (Eq a, Show a, HasXML a)
+  => Gen a -> Property
+mkpropHasXML = mkpropHasXML' id
+
+mkpropHasXML'
+  :: forall a. (Eq a, Show a, HasXML a)
+  => (a -> a) -> Gen a -> Property
+mkpropHasXML' canon gen = withTests numTests . property $ do
+  v <- forAll (canon <$> gen)
+  tripping v encodeElem (fmap canon . decodeElem @a @(Either String))
+
+mkpropHasXMLImport
+  :: forall them us. (Eq us, Show us, Show them, HasXMLImport us them)
+  => Proxy them -> Gen us -> Property
+mkpropHasXMLImport = mkpropHasXMLImport' id
+
+mkpropHasXMLImport'
+  :: forall them us. (Eq us, Show us, Show them, HasXMLImport us them)
+  => (us -> us) -> Proxy them -> Gen us -> Property
+mkpropHasXMLImport' canon _ gen = withTests numTests . property $ do
+  v <- forAll (canon <$> gen)
+  tripping v exportXml (fmap canon . importXml @us @them @(Either String))
 
 
 prop_tripNameID :: Property
-prop_tripNameID = mkprop genNameID
+prop_tripNameID = mkpropHasXML genNameID
 
 prop_tripIdPMetadata :: Property
-prop_tripIdPMetadata = mkprop genIdPMetadata
+prop_tripIdPMetadata = mkpropHasXML genIdPMetadata
 
 prop_tripSPMetadata :: Property
-prop_tripSPMetadata = mkprop genSPMetadata
+prop_tripSPMetadata = mkpropHasXML genSPMetadata
 
 prop_tripAuthnRequest :: Property
-prop_tripAuthnRequest = mkprop genAuthnRequest
+prop_tripAuthnRequest = mkpropHasXML genAuthnRequest
+
+prop_tripConditions :: Property
+prop_tripConditions = mkpropHasXML' canonicalizeConditions . Gen.prune $ genConditions
+
+prop_tripConditions' :: Property
+prop_tripConditions' = mkpropHasXMLImport' canonicalizeConditions (Proxy @HS.Conditions) . Gen.prune $ genConditions
+
+canonicalizeConditions :: Conditions -> Conditions
+canonicalizeConditions = condAudienceRestriction %~ sort . fmap NL.sort
 
 prop_tripAuthnResponse :: Property
-prop_tripAuthnResponse = mkprop . scaleGen (`div` 2) . Gen.prune $ genAuthnResponse
+prop_tripAuthnResponse = mkpropHasXML' canonicalizeAuthnResponse . Gen.prune $ genAuthnResponse
   -- without the 'prune', this triggers https://github.com/hedgehogqa/haskell-hedgehog/issues/174
+
+canonicalizeAuthnResponse :: AuthnResponse -> AuthnResponse
+canonicalizeAuthnResponse = rspPayload %~ fmap (assConditions . _Just %~ canonicalizeConditions)
