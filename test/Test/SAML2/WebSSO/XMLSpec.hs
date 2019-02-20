1248a49b70c853c0c17cdbad40f515c860c8f5b7
Matthias Fischmann <mf@zerobuzz.net>
2019-02-20 15:09:46 +0100

    Add test for encoding / escaping.

--- test/Test/SAML2/WebSSO/XMLSpec.hs
+++ test/Test/SAML2/WebSSO/XMLSpec.hs
@@ -30,11 +30,21 @@ import Util
 import qualified Data.ByteString.Base64.Lazy as EL (decodeLenient)
 import qualified Data.List as List
 import qualified Data.Map as Map
+import qualified Data.Text.Lazy as LT
 import qualified Samples
 
 -- | embed an email into a valid NameID context
-xmlWithName :: LT -> LT -> LT
-xmlWithName format email =  "<NameID xmlns:samlp=\"urn:oasis:names:tc:SAML:2.0:protocol\" xmlns:samla=\"urn:oasis:names:tc:SAML:2.0:assertion\" xmlns:samlm=\"urn:oasis:names:tc:SAML:2.0:metadata\" xmlns:ds=\"http://www.w3.org/2000/09/xmldsig#\" xmlns=\"urn:oasis:names:tc:SAML:2.0:assertion\" Format=\"" <> format <> "\">" <> email <> "</NameID>"
+xmlWithName :: Maybe LT -> LT -> LT
+xmlWithName mformat email = "<NameID " <> namespaces <> format <> ">" <> email <> "</NameID>"
+  where
+    namespaces = LT.unwords
+      [ "xmlns:samlp=\"urn:oasis:names:tc:SAML:2.0:protocol\""
+      , "xmlns:samla=\"urn:oasis:names:tc:SAML:2.0:assertion\""
+      , "xmlns:samlm=\"urn:oasis:names:tc:SAML:2.0:metadata\""
+      , "xmlns:ds=\"http://www.w3.org/2000/09/xmldsig#\""
+      , "xmlns=\"urn:oasis:names:tc:SAML:2.0:assertion\""
+      ]
+    format = maybe "" (\f -> " Format=\"" <> f <> "\"") mformat
 
 emailFormat :: LT
 emailFormat = "urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress"
@@ -50,24 +60,24 @@ spec :: Spec
 spec = describe "XML Sanitization" $ do
   describe "decodeElem" $ do
     it "should decode a valid email" $ do
-      decodeElem (xmlWithName emailFormat "somebody@example.org")
+      decodeElem (xmlWithName (Just emailFormat) "somebody@example.org")
         `shouldBe` (emailNameID "somebody@example.org" :: Either String NameID)
 
     it "should fail to decode an invalid email" $ do
-      decodeElem @NameID (xmlWithName emailFormat "&lt;somebody@example.org&gt;")
+      decodeElem @NameID (xmlWithName (Just emailFormat) "&lt;somebody@example.org&gt;")
         `shouldSatisfy` isLeft
 
     it "should decode an escaped name if format is unspecified" $ do
-      decodeElem (xmlWithName unspecifiedFormat "&lt;somebody@example.org&gt;")
+      decodeElem (xmlWithName (Just unspecifiedFormat) "&lt;somebody@example.org&gt;")
         `shouldBe` Right (unspecifiedNameID "<somebody@example.org>")
 
     it "should unescape names" $ do
-      decodeElem (xmlWithName unspecifiedFormat "&lt;somebody@example.org&gt;")
+      decodeElem (xmlWithName (Just unspecifiedFormat) "&lt;somebody@example.org&gt;")
         `shouldBe` Right (unspecifiedNameID "<somebody@example.org>")
 
     it "should not unescape more than once" $ do
       decodeElem
-        (xmlWithName unspecifiedFormat "&amp;lt;somebody@example.org&amp;gt;")
+        (xmlWithName (Just unspecifiedFormat) "&amp;lt;somebody@example.org&amp;gt;")
         `shouldBe` (mkNameID
                       (UNameIDUnspecified
                          (mkXmlText "&lt;somebody@example.org&gt;"))
@@ -76,7 +86,7 @@ spec = describe "XML Sanitization" $ do
                       Nothing :: Either String NameID)
 
     it "should not unescape text multiple times" $ do
-      decodeElem (xmlWithName entityFormat "https://www.google.com/search?q=hello&lt;world&gt;")
+      decodeElem (xmlWithName (Just entityFormat) "https://www.google.com/search?q=hello&lt;world&gt;")
         `shouldBe` (mkNameID
                       (UNameIDEntity
                          (fromRight (error "bad uri in tests")
@@ -86,4 +96,5 @@ spec = describe "XML Sanitization" $ do
                       Nothing :: Either String NameID)
 
     it "rendering doesn't double escape" $ do
-       False `shouldBe` True
+      encodeElem (unspecifiedNameID "<something>")
+        `shouldBe` (xmlWithName Nothing "&lt;something&gt;")
