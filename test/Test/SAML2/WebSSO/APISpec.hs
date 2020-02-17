7a83e300ba1e142bd4c020b728994fdfc9cb08d3
Matthias Fischmann <mf@zerobuzz.net>
2020-02-17 10:37:47 +0100

    Refactor tests.

--- test/Test/SAML2/WebSSO/APISpec.hs
+++ test/Test/SAML2/WebSSO/APISpec.hs
@@ -11,9 +11,8 @@ import Control.Monad.Except
 import Data.Either
 import Data.EitherR
 import Data.List.NonEmpty (NonEmpty((:|)))
-import Data.Maybe (maybeToList, fromJust)
+import Data.Maybe (maybeToList)
 import Data.String.Conversions
-import Network.HTTP.Types.Status (statusCode)
 import Network.Wai.Test
 import SAML2.Util
 import SAML2.WebSSO
@@ -23,15 +22,11 @@ import Servant
 import Test.Hspec hiding (pending)
 import Test.Hspec.Wai
 import Test.Hspec.Wai.Matcher
-import Text.Show.Pretty (ppShow)
 import Text.XML as XML
-import URI.ByteString as URI
 import URI.ByteString.QQ
 import Util
 
 import qualified Data.ByteString.Base64.Lazy as EL (encode, decodeLenient)
-import qualified Data.Map as Map
-import qualified Data.UUID as UUID
 import qualified Data.X509 as X509
 import qualified Data.Yaml as Yaml
 
@@ -264,58 +259,6 @@ spec = describe "API" $ do
 
 
   describe "vendor compatibility tests" $ do
-    let testAuthRespApp :: HasCallStack => URI.URI -> SpecWith (CtxV, Application) -> Spec
-        testAuthRespApp ssoURI = withapp (Proxy @("sso" :> APIAuthResp'))
-            (authresp' spissuer respuri (HandleVerdictRedirect simpleOnSuccess))
-            mkTestCtxSimple
-          where
-            spissuer = Issuer <$> respuri
-            respuri = pure ssoURI
-
-        vendorCompatibility :: HasCallStack => FilePath -> URI.URI -> Spec
-        vendorCompatibility filePath ssoURI = testAuthRespApp ssoURI $ do
-          it filePath . runtest $ \ctx -> do
-            authnrespRaw :: LT            <- readSampleIO ("vendors/" <> filePath <> "-authnresp.xml")
-            authnresp    :: AuthnResponse <- either (error . show) pure $ decode authnrespRaw
-            idpmeta      :: IdPMetadata   <- readSampleIO ("vendors/" <> filePath <> "-metadata.xml") >>=
-                                             either (error . show) pure . decode
-
-            let idpcfg = IdPConfig {..}
-                  where
-                    _idpId = IdPId UUID.nil
-                    _idpMetadata = idpmeta
-                    _idpExtraInfo = ()
-
-                -- NB: the following two bits of info are taken from the unsigned AuthnResponse
-                -- header.  the test still makes perfect sense given the information is available in
-                -- the header.  if it is not, this is legitimate.  in that case, just dig into the
-                -- assertions and take the information from there.
-
-                -- authnresp inResponseTo, with comfortable end of life.
-                reqstore :: Map.Map (ID AuthnRequest) Time
-                reqstore = Map.singleton (fromJust $ authnresp ^. rspInRespTo) timeInALongTime
-
-                -- 1 second after authnresp IssueInstant
-                now :: Time
-                now = addTime 1 $ authnresp ^. rspIssueInstant
-
-            liftIO . modifyMVar_ ctx $ \ctx' -> pure $ ctx'
-              & ctxIdPs .~ [idpcfg]
-              -- & ctxConfig . cfgSPAppURI .~ _
-              -- (the SPAppURI default is a incorrect, but that should not invalidate the test)
-              & ctxConfig . cfgSPSsoURI .~ ssoURI
-              & ctxRequestStore .~ reqstore
-              & ctxNow .~ now
-
-            -- it is essential to not use @encode authnresp@ here, as that has no signature!
-            verdict :: SResponse <- postHtmlForm "/sso/authresp"
-              [("SAMLResponse", cs . EL.encode . cs $ authnrespRaw)]
-
-            when (statusCode (simpleStatus verdict) /= 303) . liftIO $ do
-              putStrLn . ppShow . (verdict,) =<< readMVar ctx
-            liftIO $ statusCode (simpleStatus verdict) `shouldBe` 303
-
-
     vendorCompatibility "okta.com" [uri|https://staging-nginz-https.zinfra.io/sso/finalize-login|]
     -- https://developer.okta.com/signup/
 
--- test/Util.hs
+++ test/Util.hs
@@ -3,3 +3,4 @@ module Util (module X) where
 import Util.Misc as X
 import Util.TestSP as X
 import Util.Types as X
+import Util.VendorCompatibility as X
