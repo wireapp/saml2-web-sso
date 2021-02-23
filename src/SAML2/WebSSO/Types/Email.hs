f961af01eb9a2058ca141d13a94487dd4f0469eb
Matthias Fischmann <mf@zerobuzz.net>
2021-02-23 11:40:23 +0100

    Make emails case insensitive.

--- src/SAML2/WebSSO/Types/Email.hs
+++ src/SAML2/WebSSO/Types/Email.hs
@@ -3,10 +3,19 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE StrictData #-}
 
+-- | Case insensitive email type.  This helps mitigate issues with users that get casing wrong
+-- in their email addresses.
+--
+-- (The legal situation is simple: [the local part is case
+-- sensitive](https://tools.ietf.org/html/rfc5321), [the domain part is case
+-- insensitive](https://tools.ietf.org/html/rfc1035).  However, in practice, the entire email,
+-- both local and domain part, are treated as case insensitive almost everywhere.  So for
+-- interoperability we need to do the same.)
 module SAML2.WebSSO.Types.Email (Email, render, validate) where
 
 import Data.Aeson
 import Data.ByteString.Internal
+import Data.CaseInsensitive (foldCase)
 import Data.String.Conversions
 import qualified Text.Email.Validate as Email
 
@@ -17,7 +26,7 @@ render :: ConvertibleStrings ByteString s => Email -> s
 render = cs . Email.toByteString . fromEmail
 
 validate :: forall s. ConvertibleStrings s ByteString => s -> Either String Email
-validate = fmap Email . Email.validate . cs
+validate = fmap Email . Email.validate . foldCase . cs
 
 instance FromJSON Email where
   parseJSON = withText "email address" $ either fail pure . validate
